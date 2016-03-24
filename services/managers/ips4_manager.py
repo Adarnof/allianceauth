3852a99d689b5ff5a6a033ed91e93621bb39eaf9
Adarnof <adarnof@gmail.com>
2016-03-24 02:18:41 +0000

    Basic IPS4 manager  - uses REST API to add/delete/update users  - no group endpoint available yet, so all users default to Member group

---
+++ services/managers/ips4_manager.py
@@ -0,0 +1,65 @@
+import logging
+from django.conf import settings
+import requests
+import os
+
+class Ips4Manager:
+    MEMBER_GROUP_ID = 3
+    MEMBER_ENDPOINT = "/api/core/members"
+
+
+    @staticmethod
+    def __generate_random_pass():
+        return os.urandom(8).encode('hex')
+
+    @staticmethod
+    def add_user(username, email):
+        logger.debug("Adding new IPS4 user %s" % username)
+        password = Ips4Manager.__generate_random_pass()
+        data = {
+            'name': username,
+            'email': email,
+            'password': password,
+            'group': Ips4Manager.MEMBER_GROUP_ID,
+        }
+        try:
+            r = requests.POST(settings.IPS4_URL + Ips4Manager.MEMBER_ENDPOINT, auth=(settings.IPS4_API_KEY, None), json=data)
+            r.raise_for_status()
+            id = r.json()['id']
+            logger.info("Added IPS4 user %s" % username)
+            return username, password, id
+        except:
+            logger.exception("Failed to add IPS4 user %s" % username)
+            return None, None, 0
+
+    @staticmethod
+    def delete_user(id):
+        logger.debug("Deleting IPS4 user id %s" % id)
+        try:
+            r = requests.DELETE(settings.IPS4_URL + Ips4Manager.MEMBER_ENDPOINT + "/%s" % id, auth=(settings.IPS4_API_KEY, None))
+            r.raise_for_status()
+            logger.info("Deleted IPS4 user %s" % id)
+            return True
+        except:
+            logger.exception("Failed to delete IPS4 user id %s" % id)
+            return False
+
+    @staticmethod
+    def update_user_password(username, email, id, password=None):
+        logger.debug("Updating IPS4 user id %s password" % id)
+        if not password:
+            logger.debug("Generating random password")
+            password = Ips4Manager.__generate_random_pass()
+        data = {
+            'name': username, 
+            'email': email,
+            'password': password,
+        }
+        try:
+            r = requests.POST(settings.IPS4_URL + Ips4Manager.MEMBER_ENDPOINT + "/%s" % id, auth=(settings.IPS4_API_KEY, None), json=data)
+            r.raise_for_status()
+            logger.info("Reset IPS4 user %s password" % username)
+            return password
+        except:
+            logger.exception("Failed to reset IPS4 user %s password" % username)
+            return None
